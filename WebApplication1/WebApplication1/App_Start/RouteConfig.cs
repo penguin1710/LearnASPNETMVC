01a4f1c7a2b61ad057282259cd2476027a092a8b
penguin1710 <huynhnguyen.thaonhiii@gmail.com>
2019-03-07 16:57:05 +0700

    RouteConfig

--- WebApplication1/WebApplication1/App_Start/RouteConfig.cs
+++ WebApplication1/WebApplication1/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace WebApplication1
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("Welcome", "Welcome/", new { controller = "Home", action = "Index" });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
