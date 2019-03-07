d112ff4122274e126b250b4bd115141c0c0db4ff
penguin1710 <huynhnguyen.thaonhiii@gmail.com>
2019-03-07 17:45:21 +0700

    CalculatorTest

---
+++ WebApplication1/WebApplication1.Tests/Controllers/CalculatorTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1.Controllers;
+using System.Web.Mvc;
+
+namespace WebApplication1.Tests.Controllers
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            var controller = new CalculatorController();
+
+            var result = controller.Index();
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/CalculatorController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        //
+        // GET: /Calculator/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string ShowAuthor()
+        {
+            return "HNTN";
+        }
+
+        public double Factorial(int n)
+        {
+            double f = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                f = f * i;
+            }
+            return f;
+        }
+
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+	}
+}
